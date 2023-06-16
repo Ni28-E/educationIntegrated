b163fcccdc10978300524bfe56639bb3389edb7e
Heeuw <nickvanelden@gmail.com>
2023-06-16 17:38:50 +0200

    edited email form
    edited email form

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,13 +1,22 @@
-@extends('layouts.app')
+@extends('layouts.visitorApp')
 
 @section('content')
-    <form action="{{ route('store') }}" method="post">
-        @csrf
-        <p>Your Email:</p>
-        <input required type="text" name="email" id="email" value="">
-        <input required type="text" name="subject" id="subject" value="">
-        <input required type="text" name="information" id="information" value="">
-        <button type="submit">Send</button>
-    </form>
+
+
+<br>
+                <form action="{{ route('store') }}" method="post" id="email">
+                @csrf
+                <label for="email" class="form-label">Your Email </label>
+                <input required type="text" name="email" id="email" value="">
+                <label for="subject" class="form-label">Subject </label>
+                <input required type="text" name="subject" id="subject" value="">
+                <!--<input required type="text" name="information" id="information" value="">-->
+                <button type="submit">Send</button>
+                </form>
+                <textarea rows="4" cols="50" name="information" form="email" id="information">Enter text here...</textarea>
+
+
+
+
 @endsection
 
